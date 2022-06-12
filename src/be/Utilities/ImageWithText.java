8e933aaeddb932b0d27b1682f6d4a0698bd354ff
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2022-06-12 17:57:52 +0200

    Small cleanup of unused methods

--- src/be/Utilities/ImageWithText.java
+++ src/be/Utilities/ImageWithText.java
@@ -7,39 +7,10 @@ import java.io.InputStream;
 public class ImageWithText extends Image {
     private Image view;
     private String name;
-
-    public ImageWithText(String url, String name) {
-        super(url);
-        this.name = name;
-    }
-
-    public ImageWithText(String url) {
-        super(url);
-    }
-
-    public ImageWithText(String url, boolean backgroundLoading) {
-        super(url, backgroundLoading);
-    }
-
-    public ImageWithText(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth) {
-        super(url, requestedWidth, requestedHeight, preserveRatio, smooth);
-    }
-
-    public ImageWithText(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth, boolean backgroundLoading) {
-        super(url, requestedWidth, requestedHeight, preserveRatio, smooth, backgroundLoading);
-    }
-
+    
     public ImageWithText(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth, String name) {
         super(url, requestedWidth, requestedHeight, preserveRatio, smooth);
         this.name = name;
-    }
-
-    public ImageWithText(InputStream is) {
-        super(is);
-    }
-
-    public ImageWithText(InputStream is, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth) {
-        super(is, requestedWidth, requestedHeight, preserveRatio, smooth);
     }
 
     public Image getView() {
